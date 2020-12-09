c9e9bbfd1a7040b5ef5267ad042eff45f4d676f3
DESKTOP-C1U7FFO\david <david@techprescient.com>
2020-12-09 17:06:31 +0530

    seperating the root url and endpoint

--- Promises/chainApiCalls.js
+++ Promises/chainApiCalls.js
@@ -3,18 +3,24 @@
 
 const axios = require('axios');
 
+const urlJoin = require('url-join');
+
 let calls = [];
 
-let urls = [
-   {type: 'Person', endpoint: 'https://swapi.dev/api/people/1'},
-   {type: 'Film', endpoint: 'https://swapi.dev/api/films/1'},
-   {type: 'Planet', endpoint: 'https://swapi.dev/api/planets/1'},
-   {type: 'Specie', endpoint: 'https://swapi.dev/api/species/1'}
-]
+const apiUrl = 'https://swapi.dev/api';
+
+const urls = [
+   {type: 'Person', endpoint: 'people'},
+   {type: 'Film', endpoint: 'films'},
+   {type: 'Planet', endpoint: 'planets'},
+   {type: 'Specie', endpoint: 'species'},
+   {type: 'Starship', endpoint: 'starships'}
+];
 
 async function apiCall(url){
     return(new Promise((resolve, reject) => {
-        axios.get(url.endpoint).then((res) => 
+        const fullUrl = urlJoin(apiUrl, url.endpoint,'1');
+        axios.get(fullUrl).then((res) => 
         {
             let disp = `${url.type} name is ${res.data.name ?? res.data.title}`;
             calls.push(disp);
@@ -23,13 +29,17 @@ async function apiCall(url){
         }).catch((err) => console.log(err));
 }))};
 
-const promises = [apiCall(urls[0]),
+const promises = [
+    apiCall(urls[0]),
     apiCall(urls[1]),
     apiCall(urls[2]),
-    apiCall(urls[3])];
+    apiCall(urls[3])
+];
 
 Promise.all(promises).then(function(result){
-    axios.get('https://swapi.dev/api/starships/2').then((res) => {console.log(`\n4 api calls complete. 5th api call is Starship name: ${res.data.name}`);
+    const starshipUrl = urls[4];
+    const fullUrl = urlJoin(apiUrl, starshipUrl.endpoint,'2');
+    axios.get(fullUrl).then((res) => {console.log(`\n4 api calls complete. 5th api call is ${starshipUrl.type} name: ${res.data.name}`);
     console.log("\nConcatenated response: "+calls.join(". "));
     console.log("\nAll api calls completed");
 }).catch((err) => console.log(err));
