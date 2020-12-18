33a79a76732da89557f6953bcd63f83097b689a0
DESKTOP-C1U7FFO\david <david@techprescient.com>
2020-12-18 20:20:26 +0530

    arrow functions used. variables and functions renamed.

--- Promises/userPosts.js
+++ Promises/userPosts.js
@@ -1,20 +1,25 @@
-const { default: Axios } = require("axios");
 const axios = require("axios");
 
 (async () => {
-  try {
-    let users = await axios.get("https://jsonplaceholder.typicode.com/users");
 
-    users.data.forEach((user) => {
+  try {
+
+    let userResults = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+    let users = userResults.data;
+
+    users.forEach(user => {
       axios
         .get(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
-        .then((posts) => {
+        .then(postResults => {
           console.log(`User name is : ${user.name}`);
-          posts.data.forEach((post) => console.log(post.title));
+          let posts = postResults.data;
+          posts.forEach(post => console.log(post.title));
           console.log("\n");
         })
-        .catch((err) => console.log(err));
+        .catch(err => console.log(err));
     });
+
   } catch (error) {
     console.log(error);
   }
--- Promises/userPostsBatches.js
+++ Promises/userPostsBatches.js
@@ -1,43 +1,61 @@
-const axios = require('axios');
+const axios = require("axios");
 
 let rootUrl = "https://jsonplaceholder.typicode.com/posts?userId=";
 
-function* range(start, end) {
-    for(let i = start; i <= end; i++) {
-        yield i;
-    }
+// function* range(start, end) {
+//   for (let i = start; i <= end; i++) {
+//     yield i;
+//   }
+// }
+
+let range = (size, startAt = 0) => {
+    return [...Array(size).keys()].map(i => i + startAt);
 }
 
-(async () => {
-    try {
-        let users = await axios.get("https://jsonplaceholder.typicode.com/users");
-        let totalUsers = users.data.length;
-        let start = 1, end;
-        let promises;
-        while(start <= totalUsers) {
-            start = Math.min(start, totalUsers);
-            end = Math.min(start+2,totalUsers)
-            promises = [...range(start, end)].map(id => axios.get(rootUrl+id));
-            let results = await Promise.allSettled(promises);
-            results.forEach(result => {
-                if(result.status == 'fulfilled') {
-                    let posts = result.value;
 
-                    let  [post1]= posts.data.slice(0,1);
+let getBatchPosts = (results, users) => {
 
-                    console.log(users.data.find(user => user.id == post1.userId).name);
-                    
-                    posts.data.forEach(post => {
-                        console.log(post.title)
-                    });
-                    
-                    console.log("\n");
-                }
-            });
-            start += 3;
-        }
-        
-    } catch (error) {
-        console.log(error);
+  results.forEach(result => {
+
+    if (result.status == "fulfilled") {
+
+      let posts = result.value;
+      let [firstPost] = posts.data.slice(0, 1);
+      let userName = users.data.find(user => user.id == firstPost.userId).name;
+
+      console.log(`User: ${userName} published following posts: `);
+      posts.data.forEach(post => console.log(post.title));
+
+      console.log("\n");
+
     }
+
+  });
+};
+
+(async () => {
+  try {
+
+    let users = await axios.get("https://jsonplaceholder.typicode.com/users");
+    let totalUsers = users.data.length;
+    let currentUserId = 1, batchSize;
+    let promises;
+
+    while (currentUserId <= totalUsers) {
+
+      currentUserId = Math.min(currentUserId, totalUsers);
+      //end = Math.min(start + 2, totalUsers);
+      batchSize = Math.min(3, totalUsers - currentUserId);
+      //promises = [...range(start, end)].map(id => axios.get(rootUrl + id));
+      promises = range(batchSize, currentUserId).map(id => axios.get(rootUrl + id));
+      let results = await Promise.allSettled(promises); 
+      getBatchPosts(results, users);
+      currentUserId += 3;
+
+    }
+
+  } 
+  catch (error) {
+    console.log(error);
+  }
 })();
